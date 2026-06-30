86241ebcac78eb355028c961d41bdea885036be5
jeungseulki <jsk432726@gmail.com>
2026-06-30 18:06:31 +0900

    feat: 공공데이터 주차장 API 연동

---
+++ src/services/parkingService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { XMLParser } from "fast-xml-parser";
+
+const parser = new XMLParser({
+  ignoreAttributes: false,
+});
+
+export async function getParkings(query) {
+  const pageNo = query.pageNo ?? 1;
+  const numOfRows = query.numOfRows ?? 20;
+
+  const response = await axios.get(process.env.PARKING_API_URL, {
+    params: {
+      serviceKey: process.env.PARKING_API_KEY,
+      pageNo,
+      numOfRows,
+      type: "xml",
+    },
+  });
+
+  const parsed = parser.parse(response.data);
+
+  return parsed;
+}
